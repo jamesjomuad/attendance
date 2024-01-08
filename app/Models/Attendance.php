d5f0509c1f6b17fa0ba9fba42bb31459dcbfed97
James Jomuad <jjomuad@realresponse.com.au>
2024-01-08 15:34:44 +0800

    Dynamic hours computation

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -53,15 +53,34 @@ class Attendance extends Model
 
     public function getTotalHoursAttribute()
     {
-        if( $this->in_am!==null && $this->out_am === null ){
-            $total = $this->in_am->diff(Carbon::now())->h + ($this->in_am->diff(Carbon::now())->i / 60);
-            return round($total, 2);
-        }
-        elseif( $this->in_pm!==null && $this->out_pm === null )
+        $in_am  = $this->in_am!==null;
+        $out_am = $this->out_am!==null;
+        $in_pm  = $this->in_pm!==null;
+        $out_pm = $this->out_pm!==null;
+
+        if( $in_am && !$out_am && !$in_pm && !$out_pm )
         {
-            $total = $this->in_pm->diff(Carbon::now())->h + ($this->in_pm->diff(Carbon::now())->i / 60);
+            $total = $this->in_am->diff(Carbon::now())->h + ($this->in_am->diff(Carbon::now())->i / 60);    // Hour + Minutes
             return round($total, 2);
         }
+        else if( $in_am && $out_am && !$in_pm && !$out_pm )
+        {
+            $total = $this->in_am->diff($this->out_am)->h + ($this->in_am->diff($this->out_am)->i / 60);    // Hour + Minutes
+            return round($total, 2);
+        }
+        else if( $in_am && $out_am && $in_pm && !$out_pm )
+        {
+            $am_total = $this->in_am->diff($this->out_am)->h + ($this->in_am->diff($this->out_am)->i / 60);    // Hour + Minutes
+            $total = $this->in_pm->diff(Carbon::now())->h + ($this->in_pm->diff(Carbon::now())->i / 60);    // Hour + Minutes
+            return round($total + $am_total, 2);
+        }
+        else if( $in_am && $out_am && $in_pm && $out_pm )
+        {
+            $am_total = $this->in_am->diff($this->out_am)->h + ($this->in_am->diff($this->out_am)->i / 60);    // Hour + Minutes
+            $total = $this->in_pm->diff($this->out_pm)->h + ($this->in_pm->diff($this->out_pm)->i / 60);    // Hour + Minutes
+            return round($total + $am_total, 2);
+        }
+
 
         if( in_array(null, [$this->in_am,$this->out_am,$this->in_pm,$this->out_pm], true) ){
             return 0;
